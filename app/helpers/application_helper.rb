228fca02b59e9248dbe731de1935e527f3a66465
Jason Ramirez <ramirez676@gmail.com>
2015-04-04 08:47:47 -0400

    Add MarkdownProcessor class
    * Use Kramdown to parse the markdown to html. Pass the html to Nokogiri to
      access the html as an object.
    * Call markdown_to_html on welcome#index.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def markdown_to_html
+    markdown.processed_html
+  end
+
+  def markdown
+    MarkdownProcessor.new(request)
+  end
 end
---
+++ app/models/markdown_processor.rb
@@ -0,0 +1,68 @@
+require "cgi"
+require "kramdown"
+require "open-uri"
+require "pry"
+require "uri"
+
+class MarkdownProcessor
+  attr_reader :request
+
+  BASE = "https://raw.githubusercontent.com/"
+
+  def initialize(request)
+    @request = request
+  end
+
+  def processed_html
+    doc = Nokogiri::HTML(html)
+  end
+
+
+  private
+
+  def html
+    Kramdown::Document.new(unprocessed_markdown).to_html
+  end
+
+  def unprocessed_markdown
+    open(unprocessed_markdown_file).read
+  end
+
+  def unprocessed_markdown_file
+    if path_to_file then path_to_file else path_to_directory end
+  end
+
+  def path_to_file
+    if File.extname(parsed_original_url.path).present?
+      git_raw_path
+    end
+  end
+
+  def path_to_directory
+    File.join(git_raw_path, query_url)
+  end
+
+  def git_raw_path
+    File.join(BASE, local_path)
+  end
+
+  def local_path
+    parsed_original_url.path
+  end
+
+  def query_url
+    query_params["url"].first || "README.md"
+  end
+
+  def query_params
+    CGI::parse(querystring)
+  end
+
+  def querystring
+    parsed_original_url.query || ""
+  end
+
+  def parsed_original_url
+    URI::parse(request.original_url)
+  end
+end
