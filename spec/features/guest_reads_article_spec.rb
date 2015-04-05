8ed99e5a7ba1899515e1c33f3a81c618e50950df
Jason Ramirez <ramirez676@gmail.com>
2015-04-05 11:02:21 -0400

    Update feature spec for guest reading article
    Change create temp file and stub requests to take a file_name argument
    to make the feature spec more flexible.

--- spec/features/guest_reads_article_spec.rb
+++ spec/features/guest_reads_article_spec.rb
@@ -2,8 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Guest reads article" do
   it "and sees the correct html and links" do
-    create_temp_markdown_file
-    stub_request_to_return_temp_markdown_file
+    file_name = "file_one.md"
+    create_temp_markdown(file_name)
+    stub_request_to_file(file_name)
 
     visit root_path
 
@@ -16,8 +17,8 @@ RSpec.describe "Guest reads article" do
 
   private
 
-  def create_temp_markdown_file
-    File.open("/tmp/response_body.md", "w") do |file| 
+  def create_temp_markdown(file_name)
+    File.open("/tmp/#{file_name}", "w") do |file| 
       file.puts "# Hi" 
       file.puts "[link](http://www.example.com)" 
       file.puts "[link](directory/file)" 
@@ -26,8 +27,8 @@ RSpec.describe "Guest reads article" do
     end
   end
 
-  def stub_request_to_return_temp_markdown_file
+  def stub_request_to_file(file_name)
     stub_request(:any, "https://raw.githubusercontent.com/README.md")
-      .to_return(body: File.new("/tmp/response_body.md"))
+      .to_return(body: File.new("/tmp/#{file_name}"))
   end
 end
