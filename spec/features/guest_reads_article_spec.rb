06c8293df4f1d49c869c1773d2831de8d7b73177
Jason Ramirez <ramirez676@gmail.com>
2015-04-04 23:04:04 -0400

    Backfire tests for MarkdownProcessor

---
+++ spec/features/guest_reads_article_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.describe "Guest reads article" do
+  it "and sees the correct html and links" do
+    create_temp_markdown_file
+    stub_request_to_return_temp_markdown_file
+
+    visit root_path
+
+    expect(page).to have_content "Hi"
+    expect(page).to have_css "a[href='http://www.example.com']"
+    expect(page).to have_css "a[href='/directory/file']"
+    expect(page).to have_css "a[href='/file']"
+  end
+
+  private
+
+  def create_temp_markdown_file
+    File.open("/tmp/response_body.md", "w") do |file| 
+      file.puts "# Hi" 
+      file.puts "[link](http://www.example.com)" 
+      file.puts "[link](directory/file)" 
+      file.puts "[link](/file)" 
+    end
+  end
+
+  def stub_request_to_return_temp_markdown_file
+    stub_request(:any, "https://raw.githubusercontent.com/README.md")
+      .to_return(body: File.new("/tmp/response_body.md"))
+  end
+end
